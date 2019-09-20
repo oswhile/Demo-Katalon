29e90e8eaf03f3a16ede6f0f836b0a4498250049
oswhile <oswhile@gmail.com>
2019-09-20 17:50:34 -0400

    Demo Katalon Studio

---
+++ Test Suites/TSAsesoria/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f9aa8499-01f6-4df2-a706-cde8ee850813</testSuiteGuid>
+   <testCaseLink>
+      <guid>139ce3d1-9f18-4f51-9195-7cd1837b5445</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Asesoria</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
